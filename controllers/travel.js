4c2044f9e3e354312df10d71123cf6ab56035c16
dofelton <dofelton@gmail.com>
2024-07-02 17:35:19 -0500

    w3

--- controllers/travel.js
+++ controllers/travel.js
@@ -2,16 +2,12 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-    //#swagger.tags=['Travel']
     const result = await mongodb.getDb().db().collection('locations').find();
-    result.toArray((err, lists) => {
-        if(err) {
-            res.status(400).json({ message: err });
-        }
+    result.toArray().then((lists) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(locations);
     });
-}
+};
 
 const getById = (req, res) => {
     //#swagger.tags=['Travel']
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 
 router.use('/', require('./swagger'));
 
-
 router.use('/travel', require('./travel'));
 
 module.exports = router;
