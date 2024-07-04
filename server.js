da64d551c29b0ecbb805c534df819a99caafe14a
dofelton <dofelton@gmail.com>
2024-07-04 11:26:04 -0500

    Add authentication

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ const app = express();
 const port = process.env.PORT || 5000;
 const bodyParser  = require('body-parser');
 const mongodb = require('./db/connect')
+const passport = require('passport');
+const session = require('express-session');
+const GitHubStrategy = require('passport-github2').Strategy;
+const cors = require('cors');
 
 app.use(bodyParser.json());
+app.use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+}))
+app.use(passport.session())
 app.use((req,res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
@@ -14,7 +24,34 @@ app.use((req,res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
 });
+app.use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']}))
+app.use(cors({ origin: '*'}))
 app.use('/', require('./routes'));
+
+passport.use(GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL
+},
+function(accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+}));
+
+passport.serializeUser((user, done)=> {
+    done(null, user);
+})
+passport.deserializeUser((user, done)=> {
+    done(null, user);
+});
+
+app.get('/', (req, res) => { res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out")});
+
+app.get('/github/callback', passport.authenticate('github', {
+    failureRedirect: '/api-docs', session: false}),
+    (req, res) => {
+        req.session.user = req.user;
+        res.redirect('/');
+});
 
 process.on('uncaughtException', (err, origin) => {
     console.log(process.stderr.fd, `caught exception: ${err}\n` + `Exception origin: ${origin}`);
